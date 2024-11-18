9d88e07ddf19095c5d1acee0d632b85da3d694a3
Rodrigo Roussado Viegas <r.roussado2@gmail.com>
2024-11-18 11:33:47 +0000

    Update ex1-1.c
    ex 1.1, falta testar

--- ex1/ex1-1.c
+++ ex1/ex1-1.c
@@ -1,1 +1,24 @@
-
+int processFile(const char * filename, int (*action)(const char * line, void * context), void * context){
+	FILE * file;
+	char line_buffer[512];
+	int accumulator = 0;
+	
+	//abrir o ficheiro
+	file = fopen(filename, "r");
+	
+	//verificação de abertura do ficheiro
+	if(file == NULL){
+		printf("Não foi possível abrir o ficheiro.");
+		return -1; //valor de return escolhido para indicar erro
+	}
+	
+	//ler a linha
+	while(fgets(line_buffer, sizeof (line_buffer), file) != NULL){
+		//aplicar a função action à linha lida
+		accumulator += action(line_buffer, context);
+	}
+	//fechar o ficheiro
+	fclose(file);
+	
+	return accumulator;
+}
