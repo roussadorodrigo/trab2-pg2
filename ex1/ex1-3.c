fed13ab9a2a3bf9619667a9a44b213cbd9fde4f5
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2024-11-19 16:51:30 +0000

    Update ex1-3.c
    512 está agora num define
    criação da variavel do tipo FILE, afetada na mesma linha
    alteração estética de comentários e identação
    
    A correr corretamente

--- ex1/ex1-3.c
+++ ex1/ex1-3.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
+#define LINE_STRO 512												//512 caracteres de armazenamento da linha
+
 
 int processFile(const char * filename, int (*action)(const char * line, void * context), void * context){
-	FILE * file;
-	char line_buffer[512];
+	char line_buffer[LINE_STRO];
 	int accumulator = 0;
+	FILE* file = fopen(filename, "r");									//abertura o ficheiro
 	
-	//abrir o ficheiro
-	file = fopen(filename, "r");
-	
-	//verificação de abertura do ficheiro
-	if(file == NULL){
-		printf("Não foi possível abrir o ficheiro.");
+	if(file == NULL){											//verificação de abertura do ficheiro
+		printf("Não foi possível abrir o ficheiro.\n");
 		return -1; //valor de return escolhido para indicar erro
 	}
 	
-	//ler a linha
-	while(fgets(line_buffer, sizeof (line_buffer), file) != NULL){
-		//aplicar a função action à linha lida
-		accumulator += action(line_buffer, context);
+	while(fgets(line_buffer, sizeof(line_buffer), file) != NULL){						//leitura linha a linha
+		accumulator += action(line_buffer, context);							//acumulador dos valores do return da funcao "action"
 	}
-	//fechar o ficheiro
-	fclose(file);
 	
+	fclose(file);												//fechar o ficheiro
 	return accumulator;
 }
 
@@ -31,5 +26,5 @@ int linePrintRaw(const char * line, void * context){
 }
 
 int main(int argc, char * argv[]){
-	processFile(argv[1], linePrintRaw, NULL);
+	processFile(argv[1], linePrintRaw, NULL);	
 }
