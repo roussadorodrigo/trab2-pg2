b7896c1be5fa328fb5f1d70fd3253cfad5caf5fe
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2024-11-20 11:11:29 +0000

    Update ex2-5.c

--- ex2/ex2-5.c
+++ ex2/ex2-5.c
@@ -1,7 +1,7 @@
 void collSortTitle( Collection *col ){
-    for(int i = 0; i <=col->count; i++){                    //enquato ir for menor ou i
-
+    for(int i = 0; i <= col->count; i++){                    //enquato ir for menor ou i
+        qsort(col->books, col->count, sizeof(Bookdata), strcmp_ic);col->books->title[i], col->books->title[i+1]
     }
   
-
+//para usar no programa de teste, parametros de strcmp_ic; // col->books->title[i], col->books->title[i+1] \\
 }
