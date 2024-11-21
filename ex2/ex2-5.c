d3a9af5cf0c6cb9d3864e81603c347d2dc241935
Rodrigo Roussado Viegas <r.roussado2@gmail.com>
2024-11-21 13:53:51 +0000

    Delete ex2/ex2-5.c

--- ex2/ex2-5.c
+++
@@ -1,6 +0,0 @@
-void collSortTitle( Collection *col ){
-        qsort(col->books, col->count, sizeof(Bookdata), title_cmp);
-    }
-
-//for(int i = 0; i <= col->count; i++){                    //enquato ir for menor ou i
-//para usar no programa de teste, parametros de strcmp_ic; // col->books->title[i], col->books->title[i+1] \\
