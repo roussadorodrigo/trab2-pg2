2aa71a4cc8ce039912ac2e1e362110ca68e8d5a4
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2024-11-21 10:54:28 +0000

    Update ex2-5.c

--- ex2/ex2-5.c
+++ ex2/ex2-5.c
@@ -4,13 +4,3 @@ void collSortTitle( Collection *col ){
 
 //for(int i = 0; i <= col->count; i++){                    //enquato ir for menor ou i
 //para usar no programa de teste, parametros de strcmp_ic; // col->books->title[i], col->books->title[i+1] \\
-}
-
-tittle_cmp(const Bookdata *b1, const Bookdata *b2 ){
-    return strcmp_ic(b1->title[i], b2->title[i]);
-}
-
-
-isbn_cmp(const Bookdata *b1, const Bookdata *b2 ){
-    return strcmp_ic(b1->title[i], b2->title[i]);
-}
