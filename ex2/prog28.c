aa426314603a0fd1b422d473007858a92ae4624a
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2024-11-22 12:16:15 +0000

    Update prog28.c

--- ex2/prog28.c
+++ ex2/prog28.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+
+#include "books.h"
+
+Collection col;
+Collection * col_ptr = &col;
+
+int main(int argc, char * argv[]){
+	char input[LINE_LEN];
+	if(processFile(argv[1], collAddBook, col_ptr) == -1){
+		return 1;
+	}
+	
+	collSortTitle(col_ptr);
+	
+	printf("The valid commands are:\n l - show all books \n q - exit\n");
+	
+	while(1){
+		
+		scanf("%s", input);
+		
+		switch(input[0]){
+			case 'l':
+				for(int i = 0; i <= col_ptr->count-1; i++){
+				printf("TITLE: %s | ", col_ptr->books[i].title);
+				printf("ISBN: %s | ", col_ptr->books[i].isbn);
+				printf("AUTHORS: %s | ", col_ptr->books[i].authors);
+				printf("PUBLISHER: %s\n\n", col_ptr->books[i].publisher);
+				}
+				break;
+				
+				
+			case 'i':
+				char isbn[SIZE_ISBN];
+				
+				separatorUnify(input);
+				
+				strtok(input, " ");
+				isbn = strtok(input, " ");
+				
+				collSortRefIsbn(col_ptr);
+				
+				bsearch(isbn, col_ptr->refs->isbn, col_ptr->count, sizeof(), -); ///COMPLETAR!!!
+				
+				break;
+				
+			case 'q':
+				printf("Exiting the program\n");
+				return 0;
+				
+			default:
+				printf("Invalid command\n");
+				continue;
+			
+		}
+		
+	}
+	
+	
+}
 
