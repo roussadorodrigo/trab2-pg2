b0924dc827efbaab4da84d8e42f2718bedadc983
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2024-11-22 13:54:27 +0000

    Update prog28.c

--- ex2/prog28.c
+++ ex2/prog28.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "books.h"
 
@@ -7,13 +10,17 @@ Collection * col_ptr = &col;
 
 int main(int argc, char * argv[]){
 	char input[LINE_LEN];
+	char * isbn;
+	BookData * elem;
+	
 	if(processFile(argv[1], collAddBook, col_ptr) == -1){
 		return 1;
 	}
 	
 	collSortTitle(col_ptr);
+	collSortRefIsbn(col_ptr);
 	
-	printf("The valid commands are:\n l - show all books \n q - exit\n");
+	printf("The valid commands are:\n l - show all books \n q - exit\n i isbn - searches for the book by isbn\n");
 	
 	while(1){
 		
@@ -31,16 +38,23 @@ int main(int argc, char * argv[]){
 				
 				
 			case 'i':
-				char isbn[SIZE_ISBN];
+				
 				
 				separatorUnify(input);
 				
 				strtok(input, " ");
 				isbn = strtok(input, " ");
 				
-				collSortRefIsbn(col_ptr);
-				
-				bsearch(isbn, col_ptr->refs->isbn, col_ptr->count, sizeof(), -); ///COMPLETAR!!!
+				printf("%s\n", isbn);
+				elem = bsearch(isbn, col_ptr->refs, col_ptr->count, SIZE_ISBN, (int (*) (const void *, const void *))isbn_cmp);
+				if(!elem)
+					printf("ISBN not found\n");
+				else{
+					printf("TITLE: %s | ", elem->title);
+					printf("ISBN: %s | ", elem->isbn);
+					printf("AUTHORS: %s | ", elem->authors);
+					printf("PUBLISHER: %s\n\n", elem->publisher);
+				}
 				
 				break;
 				
@@ -58,4 +72,3 @@ int main(int argc, char * argv[]){
 	
 	
 }
-
