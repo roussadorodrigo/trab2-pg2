8b1c178a1b8aad54477f396292005df136892757
Rodrigo Roussado Viegas <r.roussado2@gmail.com>
2024-11-27 16:48:58 +0000

    Update prog24.c

--- ex2/prog24.c
+++ ex2/prog24.c
@@ -5,17 +5,16 @@
 int main(int argc, char * argv[]){
 	
 	Collection col;
-	Collection * col_ptr = &col;
-	col_ptr->count = 0;
+	col.count = 0;
 	
-	if(processFile(argv[1], collAddBook, col_ptr) == -1){
+	if(processFile(argv[1], collAddBook, &col) == -1){
 		return 1;
 	}
 	
-	for(int i = 0; i <= col_ptr->count-1; i++){
-		printf("%s;", col_ptr->books[i].title);
-		printf("%s;", col_ptr->books[i].isbn);
-		printf("%s;", col_ptr->books[i].authors);
-		printf("%s\n", col_ptr->books[i].publisher);
+	for(int i = 0; i <= col.count-1; i++){
+		printf("%s;", col.books[i].title);
+		printf("%s;", col.books[i].isbn);
+		printf("%s;", col.books[i].authors);
+		printf("%s\n", col.books[i].publisher);
 	}
 }
